feeba53921b1f6c3128b7c5f64686b68529bc461
AstridHG <astrid1404hg@gmail.com>
2017-02-15 19:52:22 -0600

    Modificacion

--- Practica1_EDD/src/practica1_edd/Practica1_EDD.java
+++ Practica1_EDD/src/practica1_edd/Practica1_EDD.java
@@ -5,6 +5,15 @@
  */
 package practica1_edd;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 /**
  *
  * @author Astrid Hernandez
@@ -14,10 +23,51 @@ public class Practica1_EDD {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SAXException {
         // TODO code application logic here
         Inicio inicio = new Inicio();
         inicio.setVisible(true);
-    }
     
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            DefaultHandler handler = new DefaultHandler() {
+              
+                boolean casilla = false;
+                boolean palabra = false;
+                
+                public void startElement(String uri, String localName,String qName,
+                Attributes attributes) throws SAXException {
+
+		System.out.println("Start Element :" + qName);
+
+		if (qName.equalsIgnoreCase("casilla")) {
+			casilla = true;
+		}
+
+		if (qName.equalsIgnoreCase("palabra")) {
+			palabra = true;
+		}
+
+		
+	}
+          public void endElement(String uri, String localName,String qName)throws SAXException{
+              ListaSimple listasimple = new ListaSimple();
+              listasimple.agregar(qName);
+              
+          }  
+                
+            };
+            
+            
+            
+        
+            
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(Practica1_EDD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        }
 }
+
+
